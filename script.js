0aaeaa24fae875fe89343e875836881706b23280
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-03-04 21:44:59 +0530

    ternary operater & function>arguments

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-document.write("Hello web from js.")
+// >> Test
+// document.write("Hello web from js.");
+
+// >> Ternary operater
+// const age = 18;
+// const candition = age >= 18 ? "yes" : "no";
+// > if else candition when there is only one candition after ? if candition and after : else candition
+// console.log(candition);
+
+// >> function > arguments
+// function sumOfNum() {
+//     console.log(arguments); // > this gives all args which gives in funciton calling
+//     console.log(arguments[0]);
+//     let ans = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         ans = ans + arguments[i];
+//     }
+//     return ans;
+// }
+// const sum = sumOfNum(10, 20, 30, 40, 50); // > 10, 20, 30, 40, 50 are taken by the function
+// console.log(sum);

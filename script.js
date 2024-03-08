298c1c3024d33dc09d2e797ff74a480567d9e630
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-03-08 21:08:55 +0530

    add this keywoadding high order function and callbackrk code

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // >> Test
 // document.write("Hello web from js.");
 
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 // >> Ternary operater
 // const age = 18;
 // const candition = age >= 18 ? "yes" : "no";
 // > if else candition when there is only one candition after ? if candition and after : else candition
 // console.log(candition);
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // >> function > arguments
 // function sumOfNum() {
@@ -20,24 +24,79 @@
 // const sum = sumOfNum(10, 20, 30, 40, 50); // > 10, 20, 30, 40, 50 are taken by the function
 // console.log(sum);
 
-// >> this keyword in normal funciton 
-// > an object
-const obj = {
-    value: 10,
-    myf: function(){
-        
-    }
-}
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // >> arrow function
 // > for n number of arguments.
 // > use "..." and any name you want for n numbers of arguments like "...num".
-const sum = (...num) => {
-    let ans = 0;
-    for (i = 0; i < num.length; i++) {
-        ans = ans + num[i];
-    }
-    return ans;
-};
+// const sum = (...num) => {
+//     let ans = 0;
+//     for (i = 0; i < num.length; i++) {
+//         ans = ans + num[i];
+//     }
+//     return ans;
+// };
 
-console.log(sum(5, 10, 20, 50));
+// console.log(sum(5, 10, 20, 50));
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// >> this keyword in arrow funciton
+// > an object
+// const value = 10;
+
+// const obj = {
+//     valuse2: 10,
+//     myf: () => {
+//         console.log(`${this.valuse}`); // > access the globle value
+//         console.log(`${obj.valuse2}`); // > access the object value
+//     }
+// }
+
+// obj.myf();
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// >> this keyword in normal funciton
+// > an object
+// const obj = {
+//     valuse: 10,
+//     myf: function () {
+//         console.log(this.valuse); // > access the object value
+//     }
+// }
+
+// obj.myf();
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// >> High order function & Callback (use for log process or hive tasks)
+// > High order funciton which means the function calls function as parameters
+// function add(a, b, callBack) {
+//     let result = a + b;
+//     callBack(result); // > making callBack an function and giving the value of result as paramiter
+// }
+
+// add(5, 10, (val) => { console.log(val) }); // > have to give function as paramiter using arrow function
+
+// add(10, 20, function (val) { // > in normal funtion
+//     console.log(val);
+// })
+
+// // > you can also do like this for an example
+// function adding(a, b, callBack) {
+//     let result = a + b;
+//     callBack(result);
+
+//     return () => console.log(result);
+// }
+
+// const addition = adding(2, 3, () => { }); // in this case you did not call the callBack function you store the sum in addition and it make the function
+
+// addition(); // > you pass the function in adding return thats why the addition variable becomes the function, you make your own function :> freaking cool
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
